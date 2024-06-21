ba1eeb732ab97cb3ee759d7a33c2bab16dd81f9c
Ruben E Acosta <reacosta@grupo-exito.com>
2024-06-21 14:58:50 -0500

    change config next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,13 @@ const nextConfig = {
 
     config.plugins.push(
       new NextFederationPlugin({
-        name: 'portal_web', // Nombre de tu aplicación (puede ser cualquier cadena)
-        filename: 'public/remoteEntry.js', // Ruta donde se generará el archivo de entrada remota
+        name: 'portal_web',
+        filename: 'public/remoteEntry.js',
+        library:{ type: "var", name: "portal_web" },
         exposes: {
           './Module': './src/pages/_document.tsx',
-          './button': './src/components/Button/index.tsx'
-          // Define los módulos que deseas exponer a otras aplicaciones
-          // Por ejemplo: 'components/Button'
+          './button': './src/components/Button/index.tsx',
+          './web-components': './_app.tsx',
         },
         shared: ['react', 'react-dom']
       })
